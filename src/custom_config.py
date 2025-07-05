9354a4234ecdadb7e5d16220604cde1b8a0cb169
takenX10 <frau.alessandro01@gmail.com>
2025-07-05 19:39:47 +0200

    added minor fixes to custom_logging e custom_config

--- src/custom_config.py
+++ src/custom_config.py
@@ -9,6 +9,12 @@ def check_env(key: str) -> str:
         sys.exit()
     return v
 
+def get_int(value:str)->int:
+    if not value.isdigit():
+        print(f"ERROR: {value} can't be converted as int")
+        sys.exit()
+    return int(value)
+
 dotenvpath = os.path.dirname(os.path.abspath(__file__)) + "/../.env"
 if os.path.exists(dotenvpath) and os.path.isfile(dotenvpath):
     load_dotenv(dotenvpath)
@@ -16,5 +22,6 @@ if os.path.exists(dotenvpath) and os.path.isfile(dotenvpath):
 
 SAVE_LOGS_TO_FILE = check_env("SAVE_LOGS_TO_FILE") == "true"
 LOG_FOLDER = check_env("LOG_FOLDER")
-INFO_LOG_MAX_BYTES_SIZE = check_env("INFO_LOG_MAX_BYTES_SIZE")
-ERROR_LOG_MAX_BYTES_SIZE = check_env("ERROR_LOG_MAX_BYTES_SIZE")
+INFO_LOG_MAX_BYTES_SIZE = get_int(check_env("INFO_LOG_MAX_BYTES_SIZE"))
+ERROR_LOG_MAX_BYTES_SIZE = get_int(check_env("ERROR_LOG_MAX_BYTES_SIZE"))
+BOT_TOKEN= check_env("BOT_TOKEN")
--- src/custom_logging.py
+++ src/custom_logging.py
@@ -10,62 +10,55 @@ MODULE_NAME = "ANIPUSH"
 disable_warnings(InsecureRequestWarning)
 logger: logging.Logger = logging.getLogger(__name__)
 
-class bcolors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
-    
 def format_error(x):
-    return "\033[41m\033[1m" + x + "\033[00m"
+    return "\033[41m\033[1m[ ERROR ]\033[00m"
 
 def format_warning(x):
-    return "\033[43m\033[1m" + x + "\033[00m"
+    return "\033[43m\033[1m[WARNING]\033[00m"
 
 def format_info(x):
-    return "\033[44m\033[1m" + x + "\033[00m"
+    return "\033[44m\033[1m[ INFO  ]\033[00m"
 
 def format_debug(x):
-    return "\033[47m\033[1m" + x + "\033[00m"
+    return "\033[46m\033[1m[ DEBUG ]\033[00m"
 
 class ColoredFormatter(logging.Formatter):
     def format(self, record):
         record.orig = record.msg
+        timestamp = self.formatTime(record, self.datefmt)
         if record.levelno == logging.WARNING:
-            record.msg = f"{format_warning('['+record.levelname+']')} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"{format_warning('['+record.levelname+']')} {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.ERROR:
-            record.msg = f"{format_error('['+record.levelname+']')} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"{format_error('['+record.levelname+']')} {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.INFO:
-            record.msg = f"{format_info('['+record.levelname+']')} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"{format_info('['+record.levelname+']')} {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.DEBUG:
-            record.msg = f"{format_debug('['+record.levelname+']')} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"{format_debug('['+record.levelname+']')} {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         return logging.Formatter.format(self, record)
 
 class BaseFormatter(logging.Formatter):
     def format(self, record):
-        record.orig = record.msg
+        record.msg = record.orig
+        timestamp = self.formatTime(record, self.datefmt)
         if record.levelno == logging.WARNING:
-            record.msg = f"[{record.levelname}] {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"[{record.levelname}] {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.ERROR:
-            record.msg = f"[{record.levelname}] {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"[ {record.levelname} ] {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.INFO:
-            record.msg = f"[{record.levelname}] {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"[ {record.levelname}  ] {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         elif record.levelno == logging.DEBUG:
-            record.msg = f"[{record.levelname}] {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
+            record.msg = f"[ {record.levelname} ] {timestamp} {'{'}{MODULE_NAME}-{record.funcName}{'}'} {record.msg}"
         return logging.Formatter.format(self, record)
 
 
-def set_logger(module_name, logger_level: logging._Level):
+def set_logger(module_name, logger_level):
     global MODULE_NAME
     MODULE_NAME = module_name
+    logger = logging.getLogger()
+    logger.setLevel(logger_level)
     
     # Prints to console when live executing
-    console_formatter: ColoredFormatter = ColoredFormatter()
+    console_formatter: ColoredFormatter = ColoredFormatter(datefmt='%Y-%m-%d %H:%M:%S')
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setLevel(logger_level)
     console_handler.setFormatter(console_formatter)
@@ -73,7 +66,7 @@ def set_logger(module_name, logger_level: logging._Level):
 
     # Saves to files
     if custom_config.SAVE_LOGS_TO_FILE:
-        base_formatter: BaseFormatter = BaseFormatter()
+        base_formatter: BaseFormatter = BaseFormatter(datefmt='%Y-%m-%d %H:%M:%S')
         debug_handler = RotatingFileHandler(f"{os.path.join(custom_config.LOG_FOLDER, 'debug.log')}", maxBytes=custom_config.INFO_LOG_MAX_BYTES_SIZE, backupCount=5)
         debug_handler.setFormatter(base_formatter)
         debug_handler.setLevel(logging.DEBUG)
@@ -84,7 +77,4 @@ def set_logger(module_name, logger_level: logging._Level):
         error_handler.setLevel(logging.ERROR)
         logger.addHandler(error_handler)
 
-    # Logger setup
-    logger = logging.getLogger()
-    logger.setLevel(logger_level)
-    logger.addHandler(console_handler)
+    return logger
