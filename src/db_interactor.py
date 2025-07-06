1eb25c8336f50f49cdc48834344c5ef3ce29d582
takenX10 <frau.alessandro01@gmail.com>
2025-07-06 12:21:58 +0200

    added logging on database

--- src/db_interactor.py
+++ src/db_interactor.py
@@ -1,8 +1,13 @@
 import sqlite3
 import custom_config
+from custom_logging import set_logger
+import logging
+
+log = set_logger("DATABASE_INTERACTOR", logging.DEBUG)
 
 
 def add_column(table_name, column_name, column_type):
+    log.info(f"\t\t[.] Adding column {column_name} on table {table_name}")
     conn = sqlite3.connect(custom_config.DATABASE_PATH)
     cur = conn.cursor()
     cur.execute(f"PRAGMA table_info({table_name});")
@@ -11,15 +16,22 @@ def add_column(table_name, column_name, column_type):
     if column_name not in columns:
         cur.execute(f"ALTER TABLE {table_name} ADD COLUMN {column_name} {column_type};")
         conn.commit()
+        log.info(f"\t\t[+] Column {column_name} on table {table_name} added successfully")
+    else:
+        log.info(f"\t\t[-] Column {column_name} on table {table_name} was already present")
 
     conn.close()
 
 def run_migrations():
     """Run all database migrations in order"""
+    log.info("\t[.] Running migrations commands")
     #add_column("users", "is_admin", "INTEGER DEFAULT 0")
+    log.info("\t[-] Done running migrations")
     pass
 
 def init_db():
+    log.info("[.] Initializing database")
+    log.info("\t[.] Checking and adding tables")
     conn = sqlite3.connect(custom_config.DATABASE_PATH)
     cursor = conn.cursor()
     cursor.execute("""
@@ -51,8 +63,9 @@ def init_db():
             FOREIGN KEY(user_id) REFERENCES users(id),
             FOREIGN KEY(anime_id) REFERENCES anime(id)
         );
-    """)
-    
+    """)    
     conn.commit()
     conn.close()
+    log.info("\t[-] Done checking and adding tables")
     run_migrations()
+    log.info("[-] Done initializing database")
