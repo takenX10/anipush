7f7c3992941aad48fff584a9d3992e1f58298893
takenX10 <frau.alessandro01@gmail.com>
2025-08-09 10:45:41 +0200

    added aniweb.service

--- update.sh
+++ update.sh
@@ -2,7 +2,12 @@ source /opt/anipush/venv/bin/activate
 git pull origin main
 python3 -m pip install -r requirements.txt
 systemctl stop anipush.service
-ln -f anipush.service /etc/systemd/system/.
+cp anipush.service /etc/systemd/system/.
+cp aniweb.service /etc/systemd/system/.
 systemctl daemon-reload
+systemctl stop anipush.service
 systemctl enable anipush.service
 systemctl start anipush.service
+systemctl stop aniweb.service
+systemctl enable aniweb.service
+systemctl start aniweb.service
